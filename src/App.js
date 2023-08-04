4d350b3e1db4de9ccd9626210cc574fd2ab2e765
Nogunmesa <nogunmesa@gmail.com>
2023-08-03 22:54:30 -0500

    Routed the login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
@@ -8,12 +9,14 @@ import SetUp from './pages/setup';
 import SignUp from './pages/signup';
 import Transaction from './pages/transactions';
 
+
 function App() {
   return (
       <Router>
           <Navbar />
           <Routes>
               <Route exact path='/' exact element={<Home />} />
+              <Route path='/login' element={<Login />} />
               <Route path='/setup' element={<SetUp />} />
               <Route path='/transactions' element={<Transaction />} />
               <Route path='/signup' element={<SignUp />} />
