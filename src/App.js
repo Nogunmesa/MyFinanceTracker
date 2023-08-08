62ce7ba8db6bf4c48ca814cd4c3518a843269fb4
Nogunmesa <nogunmesa@gmail.com>
2023-08-08 09:41:18 -0500

    Make Login page to be the first thing you see

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/pages';
+import Home from './components/App';
 import Login from './containers/Login';
 
 export default function App(){
     return(
         <Router>
             <Routes>
-      <Route path="/" element={<Home />} />
-            
       <Route path="/login" element={<Login />} />
+      
+      <Route path="/home" element={<Home />} />
 
             </Routes>
         </Router>
