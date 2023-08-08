04986d9871a5ffa21430ea12b6135e7f21353ba7
Nogunmesa <nogunmesa@gmail.com>
2023-08-07 22:34:09 -0500

    Make Login page to be the first thing you see

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
-import React from 'react';
-import './App.css';
-import Login from './containers/Login';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route }
-    from 'react-router-dom';
-import Home from './pages';
-import SetUp from './pages/setup';
-import SignUp from './pages/signup';
-import Transaction from './pages/transactions';
+import Login from "./containers/Login";
+<Route exact path="/login">
 
+  <Login />
 
-function App() {
-  return (
-      <Router>
-          <Navbar />
-          <Routes>
-              <Route exact path='/' element={<Home />} />
-              <Route path='/setup' element={<SetUp />} />
-              <Route path='/transactions' element={<Transaction />} />
-              <Route path='/signup' element={<SignUp />} />
-
-              <Route exact path="/login"> <Login /> </Route>
-          </Routes>
-      </Router>
-  );
-}
-
-export default App;
+</Route>
---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
+import Home from './pages';
+import SetUp from './pages/setup';
+import SignUp from './pages/signup';
+import Transaction from './pages/transactions';
+
+
+function App() {
+
+    return (
+  
+      <></>
+  
+    );
+  
+  }
+
+export default App;
