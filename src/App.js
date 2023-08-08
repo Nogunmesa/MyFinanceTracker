63145edfec3b7fd672a670de5440dc4346cde841
Nogunmesa <nogunmesa@gmail.com>
2023-08-08 09:51:06 -0500

    Checking to see login path

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ export default function App(){
         <Router>
             <Routes>
       <Route path="/login" element={<Login />} />
-      
-      <Route path="/home" element={<Home />} />
 
             </Routes>
         </Router>
