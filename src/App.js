d88c7f06e3480c01a720acbca8e0b5456a076a76
Nogunmesa <nogunmesa@gmail.com>
2023-08-09 00:48:26 -0500

    Login.js updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login'; //import login component
+import useToken from './components/App/useToken';
 import Home from './components/pages/index';
 
+
 export default function App() {
-    return (
-      <Router>
-        <Routes>
-        <Route path="/" element={<Login />} /> {/* Set up the default route */}
+  const { token, setToken } = useToken(); // Get the token and setToken from useToken
+
+  return (
+    <Router>
+      <Routes>
+        {/* Pass the setToken function as a prop to the Login component */}
+        <Route path="/" element={<Login setToken={setToken} />} />
+        <Route path="/login" element={<Login setToken={setToken} />} />
         <Route path="/home" element={<Home />} />
-        </Routes>
-      </Router>
-    );
-  }
+      </Routes>
+    </Router>
+  );
+}
