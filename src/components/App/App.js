d8b2fb4e1604466508fbbf837e82aa03d5a9ca0b
Nogunmesa <nogunmesa@gmail.com>
2023-08-13 21:05:33 -0500

    Navbar updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from '../Login/Login';
 import Home from '../pages/index';
 import SetUp from '../pages/setup';
 import SignUp from '../pages/signup';
 import Transaction from '../pages/transactions';
 import useToken from './useToken';
-import Navbar from '../Navbar';
 
 function App(){
   const {token, setToken} = useToken();
@@ -16,17 +15,18 @@ function App(){
     console.log('Null token continuing to Login')
     return <Login setToken={setToken} />
   }
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/setup" element={<SetUp />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/transactions" element={<Transaction />} />
-      </Routes>
-      </Router>
-  );
-}
-
-export default App;
+    return (
+      <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/setup" element={<SetUp />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/transactions" element={<Transaction />} />
+        </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
+  export default App;
